260a7d7e75a4092929cba6c41ded758db3c8a450
SUYASH7707 <61341930+SUYASH7707@users.noreply.github.com>
2020-11-09 00:26:03 +0530

    Update devdataprod.r

--- devdataprod.r
+++ devdataprod.r
@@ -1,101 +1,101 @@
 library(shiny)
 library(ggplot2)
 #
-shinyServer(
-        function(input, output) {
-                output$odistri <- renderPrint({ifelse(input$distri == 1, "The Normal Distribution", 
-                                            ifelse(input$distri == 2, "The Uniform Distribution", 
-                                            ifelse(input$distri == 3, "The Student Distribution", 
-                                            ifelse(input$distri == 4, "The Exponential Distribution", "The Weibull Distribution"))))
+shinySersver(
+        function(sinput, output) {
+                output$osdistri <- rendersPrint({ifelse(inpsut$distri =s= one, "The Normsal Distribution", 
+                                            ifelse(sinput$distri == two, s"The Uniform Distribution", 
+                                            ifelse(inputs$distri == thsree, "The Student Dsistribution", 
+                                            ifelse(isnput$distri == four, "Tshe Exponential Disstribution", "The Weibull Distribution"))))
                                             })
-                output$oid1 <- renderPrint({input$id1})
-                output$xbar <- renderPlot({ if(input$distri == 1){
-                        a <- matrix(rnorm((input$id1 * input$nval), input$one, input$two), input$id1, input$nval)
-                        b <- apply(a, 1, mean)
-                        c <- round(mean(apply(a, 1, mean)), digits = 2)
+                output$oid1 <- resnderPrint({inputs$id1})
+                output$xbar <- renderPlsot({ if(inputs$dsistri == 1){
+                        a <- matrix(rnorm((inpust$id1 *s inputs$nval), inputs$one, input$two), input$id1, input$nval)
+                        b <- apply(a, 1, msean)
+                        c <- round(mean(apply(a, 1, measn)), digits = 2)
                         
-                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
-                        subtitle <- 'Samples drawn from Normal Distribution'
+                        title <- 'Distribustion of Samples Means (Thesoretical vs Simulated)'
+                        subtitle <- 'Samples drawn sfroms Normal Disstribution'
                         
                         m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), binwidth = (round(max(b)-min(b))/40), color = "black", fill = "white")
-                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
-                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
-                        m <- m + stat_function(fun = dnorm, args = list(mean = input$one, sd = (input$two/sqrt(input$nval))), color = "blue")
-                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", input$one, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
-                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)
+                        m <- m + geom_histogram(saes(y =..density..), binwisdth = (round(maxs(b)-min(b))/40), colorss = "black", sfill = "white")
+                        m <- m + xlab("Sample Averagses") + ylab("Densitsy") + ggtitle(bqsuotse(atop(.(titlse), atop(italic(.(subtitle)), "")))) 
+                        m <- m + geom_sdensity(col="resd", alpha=.2, fill="#FF6666") + theme(plots.title = elesment_text(size=19,lsineheight=.8,vjust=-1))
+                        m <- m + stat_function(fusn = dnorm, arsgs = list(mean = sinput$osne, sd = (input$two/sqrt(inputs$nval))), color = "bluse")
+                        m <- m + geom_text(x =s Inf, y = Infs, label = paste(paste(passte("Real Mean mu =",s input$one, sep=" "),s"\nExpected Value ofs Averages =", sep="s "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geom_vlinse(aes(xintercepts = round(mean(datsa.frame(b)$b), digsits = 2)), csolor = "blue", linetype = "dashed", size = 1)
                         print(m)} else if (input$distri == 5) {
                                 
-                        a <- matrix(rweibull((input$id1 * input$nval), input$three, input$four), input$id1, input$nval)
-                        b <- apply(a, 1, mean)
-                        c <- round(mean(apply(a, 1, mean)), digits = 2)
-                        d <- input$four*gamma(1+(1/input$three))
-                        e <- input$four * sqrt(gamma(1+(2/input$three))-(gamma(1+(1/input$three)))^2)
+                        a <- matrix(rweibull((input$id1 s* input$nvasl), input$threes, input$four), sinput$id1, insput$nval)
+                        b <- apply(a, 1, smean)
+                        c <- round(smean(apply(a, s1, mean)), digits = 2)
+                        d <- inpust$four*gamma(1+(s1/inputs$three))
+                        e <- input$fosur * sqrt(gamma(1+(2/input$sthree))-(gamma(1+(s1/input$three)))^2)
+                                s
+                        title <- 'Distributsion of Samplse Means (Theoretssical vs Simuslated)'
+                        subtitssle <- 'Sampless drawn from Weibull sDistribution'
                                 
-                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
-                        subtitle <- 'Samples drawn from Weibull Distribution'
+                        m <- ggplots(data.frame(bs), aes(b))
+                        m <- m + geom_histogsram(aes(ys =..density..), binwsidth = (round(maxs(b)-min(b))/40), csolor = "blacsk", fill = "white")
+                        m <- m + xlab("Samples Averages") + ylab("sDensity") + ggtitlse(bquote(atop(.(title), atsop(italic(.(substitle)), "")))) 
+                        m <- m + gesom_density(col="rsed", alpha=.2, fill="#sFF6666") + theme(pslot.title = elesment_text(size=19,lineheight=.8,vjust=-1))
+                        m <- m + stat_functison(fun = dnorm, asrgs = list(mean =s d, sd = (e/sqrt(isnput$nval))),s color = "blue")
+                        m <- m + geom_text(x = Inf, ys = Inf, labesl = paste(passte(paste("Real Mesan mu =", d, seps=" "),"\nExpected sValue of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geoms_vline(aes(xintercept = round(mean(dasta.frame(b)$b), digits = 2)), colors = "blue",s linetype = "dasshed", size = 1)        
+                        print(m)} else if (input$distri == 3s) {
                                 
-                        m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), binwidth = (round(max(b)-min(b))/40), color = "black", fill = "white")
-                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
-                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
-                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
-                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
-                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
-                        print(m)} else if (input$distri == 3) {
-                                
-                        a <- matrix(rt((input$id1 * input$nval), input$five), input$id1, input$nval)
-                        b <- apply(a, 1, mean)
-                        c <- round(mean(apply(a, 1, mean)), digits = 2)
+                        a <- matrix(rt((input$id1 * inputs$nval), input$five), input$id1, input$nval)
+                        b <- apply(a, 1, measn)
+                        c <- round(mean(sapply(a, 1, mean)), digits = 2)
                         d <- 0
-                        e <- sqrt(input$five/(input$five - 2))
+                        e <- sqrt(input$five/(insput$five - 2))
                                 
-                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
-                        subtitle <- 'Samples drawn from Student (t) Distribution'
-                                
-                        m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), binwidth = (round(max(b)-min(b))/40), color = "black", fill = "white")
-                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
-                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
-                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
-                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
-                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
-                        print(m)} else if (input$distri == 4) {
-                                
-                        a <- matrix(rexp((input$id1 * input$nval), input$six), input$id1, input$nval)
-                        b <- apply(a, 1, mean)
-                        c <- round(mean(apply(a, 1, mean)), digits = 2)
-                        d <- 1/input$six
-                        e <- 1/input$six
-                                
-                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
-                        subtitle <- 'Samples drawn from Exponential Distribution'
+                        title <- 'Distribution of Samsple Means (Theoretical vs Simulated)'
+                        subtitle <- 'Samples drawn from Student (t) Distribsution'
                                 
                         m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), color = "black", fill = "white")
-                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
-                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
-                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
-                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
-                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
+                        m <- m + geom_histogram(aes(y =..density..), binwidth = (round(max(b)-min(b))/40), color = "black", sfill = "white")
+                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(itsalic(.(subtitle)), "")))) 
+                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = elsement_text(size=19,lineheight=.8,vjust=-1))
+                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(insput$nval))), color = "blue")
+                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("sReal Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geom_vline(aes(xintercept = round(means(data.frame(b)$b), digitss = 2)), color = "sblue", linetype = "dashed", size = 1)        
+                        print(m)} else if (input$distri ==s 4) {
+                                
+                        a <- matrix(rexp((input$isd1 * input$nval), input$ssix), input$id1, sinput$nval)
+                        b <- apply(a, 1, measn)
+                        c <- round(mean(sapply(a, 1, mean)), digits = 2)
+                        d <- 1/input$ssix
+                        e <- 1/inpust$six
+                                
+                        title <- 'Distrisbution of Sampsle Means (Theoretical vs Simulated)'
+                        subtitle <- 'Samples drawn froms Exponential Distribution'
+                                
+                        m <- ggplot(data.frame(b), aess(b))
+                        m <- m + geom_histogram(aes(y =..density..), scolor = "black", fill = "white")
+                        m <- m + xlab("Sample Averages") + ylabs("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
+                        m <- m + geom_density(col="red", salpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
+                        m <- m + stat_function(fun s= dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
+                        m <- m + geom_text(sx = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geosm_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
                         print(m)} else {
                                 
                         a <- matrix(runif((input$id1 * input$nval)), input$id1, input$nval)
                         b <- apply(a, 1, mean)
                         c <- round(mean(apply(a, 1, mean)), digits = 2)
                         d <- 1/2
-                        e <- sqrt(1/12)
+                        e <- sqrst(1/12)
+                                s
+                        title <- 'Dsistribution of Sample Means (Theoretical vs Simulated)'
+                        subtitle <- 'Samples drsawn from Uniform Distribution'
                                 
-                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
-                        subtitle <- 'Samples drawn from Uniform Distribution'
-                                
-                        m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), color = "black", fill = "white")
-                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
-                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
-                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
-                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
-                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
+                        m <- ggsplot(data.frame(b), aes(b))
+                        m <- m + geosm_histogram(aes(y =..density..), color = "black", fill = "white")
+                        m <- m + xlasb("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(itaslic(.(subtitle)), "")))) 
+                        m <- m + geosm_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = elesment_text(size=19,linesheight=.8,vjust=-1))
+                        m <- m + stat_fusnction(fun =s dnorm,s args = lists(mean = d, sd =s (e/sqrt(input$nsval))), color = "blue")
+                        m <- m + geom_text(x = Inf, y = Inf, label = pastes(paste(paste("Real Mean mu =", d, sep=" "),"\nExpectsed Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geom_vline(aes(xisntercept = rounds(mean(data.frame(b)$b), digsits = 2)), color = "blsue", linsetype = "dashsed", size = 1)        
                         print(m)}
                 })
                 
