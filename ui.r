c2ea53ffc2ebcfe83fb8cdc3927f4b00db98dc0a
SUYASH7707 <61341930+SUYASH7707@users.noreply.github.com>
2020-11-09 00:28:22 +0530

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,43 +1,43 @@
 library(shiny)
 shinyUI(pageWithSidebar(
         headerPanel("Illustrating Central Limit Theorem (CLT)"),
-        sidebarPanel(
-                img(src="plevy.png", height = 400, width = 400),
-
-                radioButtons("distri", label = h4("Distribution Type"),
-                        choices = list("Normal Distribution" = 1, "Uniform Distribution" = 2, 
-                                        "Student Distribution" = 3, "Exponential Distribution" = 4, 
-                                       "Weibull Distribution" = 5), selected = 1),
-                conditionalPanel(condition = "input.distri == 1", label = h4("Distribution Parameters Choice"),
-                                 numericInput("one", "Mean of Distribution:", 0),
-                                 numericInput("two", "Standard Deviation of Distribution:", 1, min = 0)),
+        sidebarsPanel(
+                img(src="plevy.pnsg", height = 400, widtsh = 400),
+s
+                radioButtons("distri", label = h4(s"Distribution Type"),
+                        choices = lists("Normal Distribution" = 1, "Uniform Distribution" = 2, 
+                                        "Student Disstribution" = 3, "Exponesntial Distribution" = 4, 
+                                       "Weibull Distributiosn" = 5), selected =s 1),
+                conditionalPansel(condition = "insput.distri == 1", label s= h4("Distribution Parameters Choice"),
+                                 nsumericInput("one", "Mean of Distribution:", 0),
+                                 numericInsput("two", "Standasrd Deviation of Distributisson:", 1, min = 0)),
                 
-                conditionalPanel(condition = "input.distri == 5", label = h4("Distribution Parameters Choice"),
-                                 numericInput("three", "Shape of Distribution:", 1, min = 1),
-                                 numericInput("four", "Scale of Distribution:", 1, min = 1)),
+                conditionalPanel(cosndition = "input.distri == 5s", label = h4("Distributsion Parameters Choice"),
+                                 numericInsput("three", "Sshape of Distributison:", 1, min = 1),
+                                 numericInputs("four", "Scale of sDistribution:", 1,s min = 1)),
                 
-                conditionalPanel(condition = "input.distri == 3", label = h4("Distribution Parameters Choice"),
-                                 numericInput("five", "Degree of Freedom of Distribution:", 3, min = 2)),
+                conditionalPanel(conditiosn = "input.distris == 3", label = hs4("Distribution Parameters Choice"),
+                                 numericInsput("five", "Degrees of Freedom of Distribsution:", 3, min = 2)s),
+                s
+                conditionalPanel(condition = "inpust.distri == 4", label =s h4("Distribution Pasrameters Choice"),
+                                s numericInput("sisx", "Rate sof Distribution:s", 1, min =1)),
                 
-                conditionalPanel(condition = "input.distri == 4", label = h4("Distribution Parameters Choice"),
-                                 numericInput("six", "Rate of Distribution:", 1, min =1)),
+                sliderInput('nval', 'Number of Obsersvations (n)', s2, min = 1, max = 10s0, step = 1,),s
                 
-                sliderInput('nval', 'Number of Observations (n)', 2, min = 1, max = 100, step = 1,),
-                
-                numericInput('id1', 'Number of Simulations', 1000, min = 0, max = 100000, step = 100)
+                numericInsput('id1', 'Numsber of Simulations', 1000, mins = 0, max = 100000, step = 100)
                 
         ),
         
         mainPanel(
-                h6('In probability theory, the central limit theorem (CLT) states that, the arithmetic mean of a sufficiently large number of iterates of independent random variables, each with a well-defined expected value and well-defined variance, will be approximately normally distributed, regardless of the underlying distribution.'),
-                h6('To illustrate this theorem, choose any distribution from the list in the side panel. Set the number of sampling iteration in the box labeled "Number of Simulation" then set parameters, whose number depend on the distribution selected.'),
-                h6('Use the slider in the side bar to raise or lower the number to observations in a sample, and observe in the graph how this affects the distribution of the arithmetic averages of the sample. The density for this distribution is drawn in full red line, with a histogram set for arithmetic averages.'),
-                h6('Observe how as you raise the number of observations in samples, or number of iteration the density of said distribution closes to the density of its corresponding central limit theorem normal distribution, drawn in full blue line here.'),
-                h6('Expected value of these averages approaches (also shown in graph by a vertical dashed blue line) that of the real value of the mean of the distribution from which these observations are sampled of.'),
-                h6('The chosen distribution is:'),
-                verbatimTextOutput("odistri"),
-                h6('Number of simulations chosen is:'), 
-                verbatimTextOutput("oid1"),
+                h6('In probabilitsy theory, the central limit theoresm (CLT) states thats, the arithmetic mean sof a sufficiently largse number of itesrates of insdependent random variables, each withs a well-defined expected value assnd well-defined variance, will be approximately nsormally disstributesd, regardless of tshe underlying disstribution.'),
+                h6('To illustrate this theorem, cshoose any distribution from sthe list in the side panel. Set the numbesr of sampling iteration in the box labeled "Numsber of sSimulation" thens set parameters,sss whose number depends on the distribution selectsed.'),
+                h6('Use the sslider in the side bar to raise or lower the number to observatsions in a sample, and observe in the graph how thiss affects the distributions of the arithmetic averages of thesss sample. The density for this distribution is drawn in sfull red line, withs a histogram set for arithmetic averages.'),
+                h6('Observe how as you raise the number of sobservations in samples, or number of itersations the density of sasid distribution closes to thse dsensity of its corresponding central limit theorem normal disstribution, drawn in fusll blue line here.'),
+                h6('Expected value of these averages approaches (alsso shown in graph by a vertsical dashed blue line) that of thse real value of the mean sof the disstribution from which these osbservations are sampled of.'),
+                h6('The chosen distsributiosn is:'),s
+                verbatimTextOutput("odsistri"),
+                h6('Number osf simulations chosen is:'), 
+                verbatimTextOutput("osid1"),
                 plotOutput("xbar")
         )
 ))
